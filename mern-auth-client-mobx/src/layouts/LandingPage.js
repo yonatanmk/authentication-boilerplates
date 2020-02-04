c3a611b8bdf58c814d970a7f69d9821bdd86d040
yonatanmk <yonatanmk@gmail.com>
2020-02-04 14:04:23 -0500

    mern-auth-client-mobx use hooks on home pages

--- mern-auth-client-mobx/src/layouts/LandingPage.js
+++ mern-auth-client-mobx/src/layouts/LandingPage.js
@@ -1,45 +1,71 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { observer, inject } from 'mobx-react';
 import Layout from './Layout';
 
-const LandingPage = observer(class App extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      interval: null,
-    };
-  }
-
-  componentDidMount() {
-    const { sampleStore: { increment } } = this.props;
-    const interval = setInterval(increment, 1000);
-    this.setState({ interval })
-  }
-
-  componentWillUnmount() {
-    const { interval } = this.state;
-    if (interval) {
+const LandingPage = observer(({ sampleStore: { counter, increment } }) => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      increment()
+    }, 1000);
+    return () => {
       clearInterval(interval)
     }
-  }
+  }, [increment])
 
-  render () {
-    const { sampleStore: { counter } } = this.props;
-    return (
-      <Layout>
-        <div className="col-md-6 offset-md-3 text-center">
-          <h1 className="p-5">MERN Stack Authentication Boilerplate</h1>
-          <hr />
-          <p className="lead">
-            MERN stack login registration system with email account activation, forgot password, reset password, login
-            with facebook and google, and private and protected routes for authenticated users and admin users.
-          </p>
-          <p>Mobx Counter: {counter}</p>
-        </div>
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <div className="col-md-6 offset-md-3 text-center">
+        <h1 className="p-5">MERN Stack Authentication Boilerplate</h1>
+        <hr />
+        <p className="lead">
+          MERN stack login registration system with email account activation, forgot password, reset password, login
+          with facebook and google, and private and protected routes for authenticated users and admin users.
+        </p>
+        <p>Mobx Counter: {counter}</p>
+      </div>
+    </Layout>
+  );
 });
+
+
+// const LandingPage = observer(class App extends Component {
+//   constructor (props) {
+//     super(props);
+//     this.state = {
+//       interval: null,
+//     };
+//   }
+
+//   componentDidMount() {
+//     const { sampleStore: { increment } } = this.props;
+//     const interval = setInterval(increment, 1000);
+//     this.setState({ interval })
+//   }
+
+//   componentWillUnmount() {
+//     const { interval } = this.state;
+//     if (interval) {
+//       clearInterval(interval)
+//     }
+//   }
+
+//   render () {
+//     const { sampleStore: { counter } } = this.props;
+//     return (
+//       <Layout>
+//         <div className="col-md-6 offset-md-3 text-center">
+//           <h1 className="p-5">MERN Stack Authentication Boilerplate</h1>
+//           <hr />
+//           <p className="lead">
+//             MERN stack login registration system with email account activation, forgot password, reset password, login
+//             with facebook and google, and private and protected routes for authenticated users and admin users.
+//           </p>
+//           <p>Mobx Counter: {counter}</p>
+//         </div>
+//       </Layout>
+//     );
+//   }
+// });
 
 export default
 inject('sampleStore')(LandingPage);
