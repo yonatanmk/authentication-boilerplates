37e50b4a7e15580db065c13556bc211a74c048c4
yonatanmk <yonatanmk@gmail.com>
2020-03-05 12:56:07 -0500

    request headers conditional spread operator

--- mern-auth-client-mobx/src/lib/request.js
+++ mern-auth-client-mobx/src/lib/request.js
@@ -3,8 +3,13 @@ import { getLocalStorage } from 'lib/auth-utils'
 
 export default params => {
   const token = getLocalStorage('token');
-  const headers = params.headers || {};
-  if (token) headers.Authorization = `Bearer ${token}`;
+  // const headers = params.headers || {};
+  // if (token) headers.Authorization = `Bearer ${token}`;
+  const headers = {
+    ...(params && params.headers),
+    ...(token && { Authorization: `Bearer ${token}` })
+  }
+  console.log({ headers })
   return axios({
     ...params,
     headers
